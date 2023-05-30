638c694273c400c240260a2b159e56f24cc4bafb
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-05-30 15:23:33 -0500

    apos

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div className='h-screen flex items-center justify-center bg-gray-800'>
       <div className='flex flex-col items-center'>
         <Image src="/netlify.svg" width={200} height={100} alt="Netlify" className='mb-10'/>
-        <h1 className='font-bold text-6xl text-white'>We're online!</h1>
+        <h1 className='font-bold text-6xl text-white'>We&apos;re online!</h1>
       </div>
     </div>
   )
