c13340f35d48111a2a8171113e05f2e0c605410b
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-05-30 15:36:45 -0500

    add time to revalidate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,31 @@
 import Image from 'next/image'
 
 export async function getStaticProps() {
+  const date = new Date();
   return {
-    props: {},
+    props: {
+      hours: date.getHours(),
+      minutes: date.getMinutes()
+    },
     revalidate: 60,
   };
 }
 
-export default function Home() {
+interface Props {
+  hours: number;
+  minutes: number;
+}
+
+const Home: React.FC<Props> = ({ hours, minutes }) => {
   return (
     <div className='h-screen flex items-center justify-center bg-gray-800'>
       <div className='flex flex-col items-center'>
         <Image src="/netlify.svg" width={200} height={100} alt="Netlify" className='mb-10'/>
-        <h1 className='font-bold text-6xl text-white'>We&apos;re online!</h1>
+        <h1 className='font-bold text-6xl text-white mb-8'>We&apos;re online!</h1>
+        <p className='text-2xl text-white'>{hours}:{minutes}</p>
       </div>
     </div>
   )
 }
+
+export default Home;
