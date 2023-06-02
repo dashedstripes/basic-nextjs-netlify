42f6975253a0a6769b689b1b084a60464df34d10
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-06-02 12:03:25 -0500

    use more readable time in odb

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,6 +1,7 @@
 import { builder, type Handler } from "@netlify/functions";
 
 const myHandler: Handler = async (event, context) => {
+  const date = new Date();
   return {
     statusCode: 200,
     ttl: 60,
@@ -11,7 +12,7 @@ const myHandler: Handler = async (event, context) => {
     <!DOCTYPE html>
       <html>
         <body>
-          Hello World ${Date.now()}
+          Hello World ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}
         </body>
     </html>
     `,
