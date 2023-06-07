285fe1cfffefb09ff1c6bb00161e0193e0127a1a
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-06-07 14:10:42 -0500

    remove timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,11 @@
 import Image from 'next/image'
 
-export async function getStaticProps() {
-  const date = new Date();
-  return {
-    props: {
-      hours: date.getHours(),
-      minutes: date.getMinutes(),
-      seconds: date.getSeconds()
-    },
-    revalidate: 60,
-  };
-}
-
-interface Props {
-  hours: number;
-  minutes: number;
-  seconds: number;
-}
-
-const Home: React.FC<Props> = ({ hours, minutes, seconds }) => {
+const Home: React.FC = () => {
   return (
     <div className='h-screen flex items-center justify-center bg-gray-800'>
       <div className='flex flex-col items-center'>
         <Image src="/netlify.svg" width={200} height={100} alt="Netlify" className='mb-10'/>
         <h1 className='font-bold text-6xl text-white mb-8'>We&apos;re online!</h1>
-        <p className='text-2xl text-white'>{hours}:{minutes}:{seconds}</p>
       </div>
     </div>
   )
