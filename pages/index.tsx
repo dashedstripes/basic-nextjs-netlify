f54c30ff809cccf9212b1e1724dbf82acfd8f275
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-06-07 14:19:08 -0500

    update text to deployed

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
     <div className='h-screen flex items-center justify-center bg-gray-800'>
       <div className='flex flex-col items-center'>
         <Image src="/netlify.svg" width={200} height={100} alt="Netlify" className='mb-10'/>
-        <h1 className='font-bold text-6xl text-white mb-8'>We&apos;re online!</h1>
+        <h1 className='font-bold text-6xl text-white mb-8'>We&apos;re deployed!</h1>
       </div>
     </div>
   )
