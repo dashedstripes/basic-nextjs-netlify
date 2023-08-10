948ba20e0e5354f464c19640b67706ab693776f8
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-08-10 11:11:00 -0500

    more movin

--- plugins/netlify-plugin-email-sendgrid/index.js
+++ plugins/netlify-plugin-email-sendgrid/index.js
@@ -5,7 +5,7 @@ module.exports = {
       const fs = require("fs")
       const path = require('path')
 
-      const filenames = fs.readdirSync(path.join(__dirname, '..'));
+      const filenames = fs.readdirSync(path.join(__dirname, '..', '..'));
   
       console.log("\nCurrent directory filenames:");
       filenames.forEach(file => {
