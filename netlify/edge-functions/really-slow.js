ab09c8206918a4ab181d338234c325dfa1434b91
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-09-29 13:50:57 -0500

    break it even more, cpu style

--- netlify/edge-functions/really-slow.js
+++ netlify/edge-functions/really-slow.js
@@ -1,7 +1,9 @@
 export default async (_, context) => {
   const response = await context.next();
 
-  await new Promise(r => setTimeout(r, 20000));
+  for(let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
+    console.log(i);
+  }
 
   return response;
 }
